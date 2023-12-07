9885e502776a7555a1451f1f6844715651991477
ouharri <ouharrioutman@gmail.com>
2023-12-07 09:37:11 +0100

    progress

---
+++ src/app/pages/responsable/home/home.component.ts
@@ -0,0 +1,90 @@
+import {Component, Inject, Injector, NO_ERRORS_SCHEMA} from '@angular/core';
+import {PromotionService} from "../../../core/services/promotion.service";
+import {Promotion} from "../../../core/model/promotion";
+import {SideBarComponent} from "../../../shared/components/side-bar/side-bar.component";
+import {CommonModule} from '@angular/common';
+import {LoginComponent} from "../../authentication/login/login.component";
+import {TestComponent} from "../../../shared/components/test/test.component";
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {TuiDataListModule, TuiDialogService} from '@taiga-ui/core';
+import {TuiDataListWrapperModule, TuiSelectModule} from '@taiga-ui/kit';
+
+@Component({
+    selector: 'app-home',
+    standalone: true,
+    imports: [
+        SideBarComponent,
+        CommonModule,
+        LoginComponent,
+        TestComponent,
+        FormsModule,
+        ReactiveFormsModule,
+        TuiSelectModule,
+        TuiDataListModule,
+        TuiDataListWrapperModule,
+    ],
+    templateUrl: './home.component.html',
+    schemas: [NO_ERRORS_SCHEMA],
+})
+export class HomeComponent {
+    constructor(private promotion: PromotionService,
+                @Inject(TuiDialogService) private readonly dialogs: TuiDialogService,
+                @Inject(Injector) private readonly injector: Injector) {
+    }
+
+    showDialog(): void {
+        this.dialogs
+            .open(
+                {
+                    size: 'l',
+                    closeable: true,
+                    dismissible: true,
+                },
+            )
+            .subscribe();
+    }
+
+    ngOnInit(): void {
+        this.getTotalPage();
+        this.getAllPromotions();
+    }
+
+    loopArray = new Array(0);
+    currentPage: number = 1;
+    promotions: Promotion[] = []
+    totalPage: number = 0;
+
+    getAllPromotions() {
+        this.promotion.getAllPromotions(this.currentPage).subscribe((data: any) => {
+            this.promotions = data;
+        });
+    }
+
+    getPromotionsByPage() {
+        this.promotion.getAllPromotions(this.currentPage).subscribe((data: any) => {
+            this.promotions = data;
+        });
+    }
+
+    previousPage() {
+        this.currentPage--;
+        this.getPromotionsByPage();
+    }
+
+    nextPage() {
+        this.currentPage++;
+        this.getPromotionsByPage();
+    }
+
+    getTotalPage() {
+        this.promotion.getTotalPage().subscribe((data: any) => {
+            this.totalPage = data;
+            this.loopArray = new Array(this.totalPage);
+        });
+    }
+
+    changePage(param: any) {
+        this.currentPage = param;
+        this.getPromotionsByPage();
+    }
+}
